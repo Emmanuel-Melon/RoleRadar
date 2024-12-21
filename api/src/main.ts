6d5fa286a024899ad1303aaecc6d05d615aaa5ef
= <emmanuelgatwech@gmail.com>
2024-12-21 17:20:41 +0300

    feat: update port

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
@@ -11,6 +13,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(8000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
